c755858dd1ea13949318f767f23d9c5912393a6f
parkjungkwan <pakjkwan@gmail.com>
2021-08-02 14:30:22 +0900

    14

--- tests/titanic_view_test.py
+++ tests/titanic_view_test.py
@@ -11,12 +11,7 @@ class TitanicViewTest(unittest.TestCase):
         print(f'The head of Train is \n {this.train.head(2)}')
         print(f'The head of Test is \n {this.test.head(2)}')
 
-    def test_preprocessing(self) -> object:
-        service = self.service
-        this = self.dataset
-        this.train = service.new_model("train")
-        this.test = service.new_model("test")
-        return this
+
 
 if __name__ == '__main__':
     unittest.main()
