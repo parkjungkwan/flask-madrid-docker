b9acad0ca9e343241abe13304bb2a476837693f6
parkjungkwan <pakjkwan@gmail.com>
2021-10-07 10:59:56 +0900

    10

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +10,5 @@ def hello():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
+
--- basic/one_to_ten_sum.py
+++ basic/one_to_ten_sum.py
@@ -1,13 +1,28 @@
+'''
+파이썬에서 루프 형태는 3가지가 있다.
+'''
+from icecream import ic
 class OneToTenSum(object):
+    # 예시 1
     def one_to_ten_sum_1(self):
         sum = 0
         for i in range(1, 11):
             sum += i
-        print(sum)
+        return sum
 
+    # 예시 2
     def one_to_ten_sum_2(self):
-        print(sum(i for i in range(1,11)))
+        val = sum(i for i in range(1,11))
+        return val
 
-
+    # 예시 3
     def one_to_ten_sum_3(self):
-        print(sum(range(1, 11)))
+        val = sum(range(1, 11))
+        return val
+
+
+if __name__ == '__main__':
+    a = OneToTenSum()
+    ic(a.one_to_ten_sum_1())
+    ic(a.one_to_ten_sum_2())
+    ic(a.one_to_ten_sum_3())
